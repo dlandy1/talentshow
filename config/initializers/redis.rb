04c71d1c62fc1ef0da967dda0c7d1a6e2fbffe92
dlandy1 <dlandy1@babson.edu>
2015-05-27 20:55:35 -0400

    Updated Gemfile for deployment to Heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,3 @@
- if Rails.env == "development"
-    REDIS =Redis.new
-  else
-    uri = URI.parse(ENV["REDISTOGO_URL"])
-    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  end
+
+    REDIS = Redis.new
+
