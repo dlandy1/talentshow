84d5d35cba506ba0365881c5c1573918286de241
dlandy1 <dlandy1@babson.edu>
2015-05-29 19:35:44 -0400

    mvp 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,6 @@ class User < ActiveRecord::Base
    TEMP_EMAIL_PREFIX = 'change@me'
   TEMP_EMAIL_REGEX = /\Achange@me/
 
-    
-
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
-  validates :slug, uniqueness: true
-
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
