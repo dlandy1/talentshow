f8b7de8cb3af436948cd751338b703c6157e512a
dlandy1 <dlandy1@babson.edu>
2015-06-05 22:02:58 -0400

    email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ActionMailer::Base
   end
 
   def news
-    @posts = Post.where(:accepted => true).where("created_at >= ?", Time.zone.now.beginning_of_day).order('rank DESC')
+    @posts = Post.where(:accepted => true).where("created_at >= ?", Time.zone.now.beginning_of_day).order('rank DESC').first(3)
     mail(to: '', bcc: Newsletter.all.collect(&:email), subject: 'Top Performers of the day')
 
   end
