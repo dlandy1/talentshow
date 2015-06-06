ac4c1cec1c04448860ea8dada927dbcc367f15c9
dlandy1 <dlandy1@babson.edu>
2015-06-06 00:35:45 -0400

    sttyle

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,2 @@
- if Rails.env == "development"
-    REDIS =Redis.new
-  else
-    uri = URI.parse(ENV["REDISTOGO_URL"])
-    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  end
 
+    REDIS =Redis.new
