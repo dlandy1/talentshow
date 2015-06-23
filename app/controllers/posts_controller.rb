4e05da472ba9fdb323e28f17fa5640980d622ba7
dlandy1 <dlandy1@babson.edu>
2015-06-22 23:13:47 -0400

    fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
     @voters = Kaminari.paginate_array(@post.voters).page(params[:page]).per(20)
     @comment = Comment.new
     @post_groups= Post.order('rank DESC').group_by{|post| [post.created_at.wday, post.created_at.to_date]}
-    @sorted_groups = @post_groups.sort_by { |name, age| age.first.created_at }.reverse
+    @sorted_groups =Kaminari.paginate_array(@post_groups.sort_by { |name, age| age.first.created_at }.reverse).page(params[:page]).per(5)
     respond_with(@post) do |format|
     end
   end
